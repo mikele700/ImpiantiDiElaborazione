00f5132545ea10cb790806bb8b55fe362663226b
davide-trimaldi <davide.trimaldi@gmail.com>
2019-02-07 23:20:06 +0100

    correzioni

--- Ex4/reliability_MTTF_es5.m
+++ Ex4/reliability_MTTF_es5.m
@@ -11,12 +11,13 @@ MTTF=[ 5000; %processing unit
 lambda=1./MTTF;
 Rm=exp(-lambda*1);
 
-Rsys=(1-(1-Rm(1))^2)*((1-(1-Rm(6))^2))^2*(1-(1-Rm(2))^2)*(1-(1-Rm(4)))*(Rm(5)*(1-(1-Rm(3))^3));
+Rsys=(1-(1-Rm(1))^2)*((1-(1-Rm(6))^2)^2)*(1-(1-Rm(2))^2)*(1-((1-Rm(4))*(1-(Rm(5)*(1-(1-Rm(3))^3)))));
 
 
 %calcolo la coverage
-Rsys_noproc =((1-(1-Rm(6))^2))^2*(1-(1-Rm(2))^2)*(1-(1-Rm(4)))*(Rm(5)*(1-(1-Rm(3))^3));
-Rproc_mod= 0.999999/Rsys_noproc;
-c=(Rproc_mod-Rm(1))/((1-Rm(1))*Rm(1));
+Rsys_noproc =((1-(1-Rm(6))^2))^2*(1-(1-Rm(2))^2)*(1-((1-Rm(4))*(1-(Rm(5)*(1-(1-Rm(3))^3)))));
+c=(0.99999-(Rm(1)*Rsys_noproc))/(Rsys_noproc*Rm(1)*(1-Rm(1)));
 
+%Rproc_mod= 0.99999/Rsys_noproc;
+%c=(Rproc_mod-Rm(1))/((1-Rm(1))*Rm(1));
  
